48498a42578f9f369b1496570139f8c1950da392
Ian Kent <email@iankent.co.uk>
2016-10-18 08:27:04 +0100

    add timeout handler to alice, and configure server timeouts

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/ONSdigital/dp-frontend-renderer/assets"
 	"github.com/ONSdigital/dp-frontend-renderer/handlers/homepage"
 	"github.com/ONSdigital/dp-frontend-renderer/render"
 	"github.com/ONSdigital/go-ns/handlers/healthcheck"
 	"github.com/ONSdigital/go-ns/handlers/requestID"
+	"github.com/ONSdigital/go-ns/handlers/timeout"
 	"github.com/ONSdigital/go-ns/log"
 	"github.com/gorilla/pat"
 	"github.com/justinas/alice"
@@ -19,7 +21,7 @@ import (
 func main() {
 	bindAddr := os.Getenv("BIND_ADDR")
 	if len(bindAddr) == 0 {
-		bindAddr = ":8080"
+		bindAddr = ":8081"
 	}
 
 	debugMode, _ := strconv.ParseBool(os.Getenv("DEBUG"))
@@ -33,14 +35,25 @@ func main() {
 	})
 
 	router := pat.New()
-	alice := alice.New(log.Handler, requestID.Handler(16)).Then(router)
+	alice := alice.New(
+		timeout.Handler(10*time.Second),
+		log.Handler,
+		requestID.Handler(16),
+	).Then(router)
 
 	router.Get("/healthcheck", healthcheck.Handler)
 	router.Post("/homepage", homepage.Handler)
 
 	log.Debug("Starting server", log.Data{"bind_addr": bindAddr})
 
-	if err := http.ListenAndServe(bindAddr, alice); err != nil {
+	server := &http.Server{
+		Addr:         bindAddr,
+		Handler:      alice,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
 		log.Error(err, nil)
 		os.Exit(1)
 	}
