452d0ebd210871297a1acdb3ac4cc38c951d7bfe
Crispioso <crispin.merriman@gmail.com>
2016-10-21 15:22:57 +0100

    Move generic handler out of homepage handler

--- handlers/homepage/handler.go
+++ handlers/homepage/handler.go
@@ -1,19 +1,10 @@
 /*Package homepage ...*/
 package homepage
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
+import "net/http"
 
-	"github.com/ONSdigital/dp-frontend-renderer/config"
-	"github.com/ONSdigital/dp-frontend-renderer/model"
-	"github.com/ONSdigital/dp-frontend-renderer/render"
-	"github.com/ONSdigital/go-ns/log"
-)
-
-//Handler2 ...
-func Handler2(w http.ResponseWriter, req *http.Request) {
+//Handler ...
+func Handler(w http.ResponseWriter, req *http.Request) {
 	var page Page
 
 	Handler(w, req, &page, &page.Page, "homepage", func() {
@@ -27,48 +18,4 @@ func Handler2(w http.ResponseWriter, req *http.Request) {
 			}
 		}
 	})
-}
-
-//Handler ...
-func Handler(w http.ResponseWriter, req *http.Request, page interface{}, page2 *model.Page, templateName string, f func()) {
-	b, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		render.JSON(w, 400, model.ErrorResponse{
-			Error: err.Error(),
-		})
-		log.ErrorR(req, err, nil)
-		return
-	}
-
-	req.Body.Close()
-
-	err = json.Unmarshal(b, &page)
-	if err != nil {
-		render.JSON(w, 400, model.ErrorResponse{
-			Error: err.Error(),
-		})
-		log.ErrorR(req, err, nil)
-		return
-	}
-
-	f()
-
-	page2.Language = req.Header.Get("Accept-Language")
-	if page2.Language != "en" && page2.Language != "cy" {
-		page2.Language = "en"
-	}
-
-	page2.PatternLibraryAssetsPath = config.PatternLibraryAssetsPath
-
-	page2.SiteDomain = config.SiteDomain
-
-	log.DebugR(req, "rendered template", log.Data{"template": templateName})
-	err = render.HTML(w, 200, templateName, page)
-	if err != nil {
-		render.JSON(w, 500, model.ErrorResponse{
-			Error: err.Error(),
-		})
-		log.ErrorR(req, err, nil)
-		return
-	}
 }
--- handlers/productPage/model.go
+++ handlers/productPage/model.go
@@ -4,16 +4,8 @@ import "github.com/ONSdigital/dp-frontend-renderer/model"
 
 //Page contains data re-used for each page type a Data struct for data specific to the page type
 type Page struct {
-	Type                     string               `json:"type"`
-	URI                      string               `json:"uri"`
-	Taxonomy                 []model.TaxonomyNode `json:"taxonomy"`
-	Breadcrumb               []model.TaxonomyNode `json:"breadcrumb"`
-	ServiceMessage           string               `json:"serviceMessage"`
-	Metadata                 model.Metadata       `json:"metadata"`
-	SiteDomain               string               `json:"-"`
-	PatternLibraryAssetsPath string               `json:"-"`
-	Language                 string               `json:"-"`
-	Data                     ProductPage          `json:"data"`
+	model.Page
+	Data ProductPage `json:"data"`
 }
 
 //ProductPage contains data specific to this page type
