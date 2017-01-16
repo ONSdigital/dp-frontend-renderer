6fcd048d83974a3e6b37bcce864ce435b2fb37d8
Marek <mkarwowski@equalexperts.com>
2017-01-16 15:08:54 +0000

    Configuraiton update to point to latest js build

--- config/config.go
+++ config/config.go
@@ -17,7 +17,7 @@ var DataDiscovery = struct {
 	JOB_API_URL string
 	BASE_PATH   string
 }{
-	"https://cdn.ons.gov.uk/dp-dd-react-app/4ca82ca",
+	"https://cdn.ons.gov.uk/dp-dd-react-app/0ccf5a9",
 	"/dd/api",
 	"/dd/api/jobs",
 	"/dd",
