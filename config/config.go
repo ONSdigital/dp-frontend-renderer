83e86a6731ad802cc6dada4144a08de49d84d76a
Marek <mkarwowski@equalexperts.com>
2017-01-16 17:08:49 +0000

    App config update to point to latest (d20de29) js build

--- config/config.go
+++ config/config.go
@@ -17,7 +17,7 @@ var DataDiscovery = struct {
 	JOB_API_URL string
 	BASE_PATH   string
 }{
-	"https://cdn.ons.gov.uk/dp-dd-react-app/4d74a75",
+	"https://cdn.ons.gov.uk/dp-dd-react-app/d20de29",
 	"/dd/api",
 	"/dd/api/jobs",
 	"/dd",
