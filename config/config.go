ae3e76227156bdb230320d227b8b91840c5cef54
Marek <mkarwowski@equalexperts.com>
2017-01-17 11:29:05 +0000

    React app update to latest (9b52ce9) commit

--- config/config.go
+++ config/config.go
@@ -17,7 +17,7 @@ var DataDiscovery = struct {
 	JOB_API_URL string
 	BASE_PATH   string
 }{
-	"https://cdn.ons.gov.uk/dp-dd-react-app/c3dba38",
+	"https://cdn.ons.gov.uk/dp-dd-react-app/9b52ce9",
 	"/dd/api",
 	"/dd/api/jobs",
 	"/dd",
