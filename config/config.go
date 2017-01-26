4eed6e5793c54e9bd6bc2b8ec9d2365af3cb9ce5
Marek <mkarwowski@equalexperts.com>
2017-01-26 11:55:10 +0000

    Config updated to fetch latest js bundle (bdde8d9)

--- config/config.go
+++ config/config.go
@@ -17,7 +17,7 @@ var DataDiscovery = struct {
 	JOB_API_URL string
 	BASE_PATH   string
 }{
-	"https://cdn.ons.gov.uk/dp-dd-react-app/0eb4628",
+	"https://cdn.ons.gov.uk/dp-dd-react-app/bdde8d9",
 	"/dd/api",
 	"/dd/api/jobs",
 	"/dd",
