09b60376abc14f958463ec022d9cd2bdbaea80c2
Crispioso <crispin.merriman@gmail.com>
2017-03-03 14:36:40 +0000

    Add flag for hiding header and footer on splash page

--- vendor/github.com/ONSdigital/dp-frontend-models/model/page.go
+++ vendor/github.com/ONSdigital/dp-frontend-models/model/page.go
@@ -2,14 +2,14 @@ package model
 
 //Page contains data re-used for each page type a Data struct for data specific to the page type
 type Page struct {
-	Type                             string         `json:"type"`
-	URI                              string         `json:"uri"`
-	Taxonomy                         []TaxonomyNode `json:"taxonomy"`
-	Breadcrumb                       []TaxonomyNode `json:"breadcrumb"`
-	ServiceMessage                   string         `json:"serviceMessage"`
-	Metadata                         Metadata       `json:"metadata"`
-	SiteDomain                       string         `json:"-"`
-	PatternLibraryAssetsPath         string         `json:"-"`
-	Language                         string         `json:"-"`
-	IncludeAssetsIntegrityAttributes bool           `json:"-"`
+	Type                     string         `json:"type"`
+	URI                      string         `json:"uri"`
+	Taxonomy                 []TaxonomyNode `json:"taxonomy"`
+	Breadcrumb               []TaxonomyNode `json:"breadcrumb"`
+	ServiceMessage           string         `json:"serviceMessage"`
+	Metadata                 Metadata       `json:"metadata"`
+	SiteDomain               string         `json:"-"`
+	PatternLibraryAssetsPath string         `json:"-"`
+	Language                 string         `json:"-"`
+	HideHeaderAndFooter      bool         `json:"-"`
 }
