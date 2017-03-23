8bd162b8410d51d54d0248d427b8d1dc467d6146
Crispioso <crispin.merriman@gmail.com>
2017-03-23 08:14:21 +0000

    Update to use latest React app (bcba0c5)

--- config/config.go
+++ config/config.go
@@ -16,7 +16,7 @@ var DataDiscovery = struct {
 	JobsApiURL         string
 	BasePath           string
 }{
-	"https://cdn.ons.gov.uk/dp-dd-react-app/e50b55f",
+	"https://cdn.ons.gov.uk/dp-dd-react-app/bcba0c5",
 	"/dd/api",
 	"/dd/api/jobs",
 	"/dd",
