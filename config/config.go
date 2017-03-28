1510f1a11050ca89f2145b96f22ad6468f595bd9
Crispioso <crispin.merriman@gmail.com>
2017-03-28 09:06:41 +0100

    Update to latest React app (caf1000)

--- config/config.go
+++ config/config.go
@@ -16,7 +16,7 @@ var DataDiscovery = struct {
 	JobsApiURL         string
 	BasePath           string
 }{
-	"https://cdn.ons.gov.uk/dp-dd-react-app/6fe6107",
+	"https://cdn.ons.gov.uk/dp-dd-react-app/caf1000",
 	"/dd/api",
 	"/dd/api/jobs",
 	"/dd",
