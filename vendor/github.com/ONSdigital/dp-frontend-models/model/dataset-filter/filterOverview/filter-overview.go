91cd5cf1fa80a1cbab5674d9451cd128556aa3a0
Matt Rout <matt_rout92@live.co.uk>
2017-07-18 14:18:32 +0100

    Update job_id to become filter_id

--- vendor/github.com/ONSdigital/dp-frontend-models/model/dataset-filter/filterOverview/filter-overview.go
+++ vendor/github.com/ONSdigital/dp-frontend-models/model/dataset-filter/filterOverview/filter-overview.go
@@ -5,8 +5,8 @@ import "github.com/ONSdigital/dp-frontend-models/model"
 // Page ...
 type Page struct {
 	model.Page
-	Data  FilterOverview `json:"data"`
-	JobID string         `json:"job_id"`
+	Data     FilterOverview `json:"data"`
+	FilterID string         `json:"filter_id"`
 }
 
 // FilterOverview ...
