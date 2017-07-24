94842f04bc032dd510a5b97c01a28829c08c9d7d
Matt Rout <matt_rout92@live.co.uk>
2017-07-24 10:32:09 +0100

    Add frontend model to preview page template

--- vendor/github.com/ONSdigital/dp-frontend-models/model/dataset-filter/previewPage/preview-page.go
+++ vendor/github.com/ONSdigital/dp-frontend-models/model/dataset-filter/previewPage/preview-page.go
@@ -10,5 +10,22 @@ type Page struct {
 
 // PreviewPage ...
 type PreviewPage struct {
-	FilterID string `json:"filter_id"`
+	FilterID           string      `json:"filter_id"`
+	Downloads          []Download  `json:"downloads"`
+	Dimensions         []Dimension `json:"dimensions"`
+	DatasetDescription string      `json:"dataset_description"`
+	DatasetTitle       string      `json:"dataset_title"`
+}
+
+// Download has the details for an individual downloadable files
+type Download struct {
+	Extension string `json:"extension"`
+	Size      string `json:"size"`
+	URI       string `json:"uri"`
+}
+
+// Dimension ...
+type Dimension struct {
+	Title  string   `json:"title"`
+	Values []string `json:"values"`
 }
