f28d2f6ac7e47bc40f6fa4affcc27646b908ddde
boxadesign <info@boxadesign.co.uk>
2017-08-01 15:18:02 +0100

    Added a condition to fake loading of data and browser refresh

--- vendor/github.com/ONSdigital/dp-frontend-models/model/dataset-filter/previewPage/preview-page.go
+++ vendor/github.com/ONSdigital/dp-frontend-models/model/dataset-filter/previewPage/preview-page.go
@@ -5,10 +5,26 @@ import "github.com/ONSdigital/dp-frontend-models/model"
 // Page ...
 type Page struct {
 	model.Page
-	Data PreviewPage `json:"data"`
+	Data            PreviewPage `json:"data"`
+	IsContentLoaded bool        `json:"is_content_loaded"`
 }
 
 // PreviewPage ...
 type PreviewPage struct {
-	FilterID string `json:"filter_id"`
+	FilterID   string      `json:"filter_id"`
+	Downloads  []Download  `json:"downloads"`
+	Dimensions []Dimension `json:"dimensions"`
+}
+
+// Download has the details for an individual downloadable files
+type Download struct {
+	Extension string `json:"extension"`
+	Size      string `json:"size"`
+	URI       string `json:"uri"`
+}
+
+// Dimension ...
+type Dimension struct {
+	Name   string   `json:"name"`
+	Values []string `json:"values"`
 }
