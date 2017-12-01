26f81eda7003a861977466e7079d093a659d7cff
Matt Rout <matt_rout92@live.co.uk>
2017-12-01 14:50:47 +0000

    Update sixteens commit

--- config/config.go
+++ config/config.go
@@ -7,4 +7,4 @@ var DebugMode bool
 var SiteDomain = "ons.gov.uk"
 
 // PatternLibraryAssetsPath is the URL to the CSS and JS assets from the pattern library
-var PatternLibraryAssetsPath = "//cdn.ons.gov.uk/sixteens/20da576"
+var PatternLibraryAssetsPath = "//cdn.ons.gov.uk/sixteens/7b78ccc"
