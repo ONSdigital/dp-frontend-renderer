33e7e0b443a6ce8bdc3957ef8045c782a2d135c1
mattrout92 <matt_rout92@live.co.uk>
2017-12-05 13:59:09 +0000

    Update feedback form

--- handlers/feedback/handler.go
+++ handlers/feedback/handler.go
@@ -3,12 +3,12 @@ package feedback
 import (
 	"net/http"
 
-	"github.com/ONSdigital/dp-frontend-models/model"
+	"github.com/ONSdigital/dp-frontend-models/model/feedback"
 	"github.com/ONSdigital/dp-frontend-renderer/render"
 )
 
 func Handler(w http.ResponseWriter, req *http.Request) {
-	var page model.Page
+	var page feedback.Page
 
-	render.Handler(w, req, &page, &page, "feedback", nil)
+	render.Handler(w, req, &page, &page.Page, "feedback", nil)
 }
