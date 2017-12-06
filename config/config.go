ddded88f2f4df1e29037aa76f68c036460c2ce7b
boxadesign <info@boxadesign.co.uk>
2017-12-06 12:26:50 +0000

    update 16's path

--- config/config.go
+++ config/config.go
@@ -7,4 +7,4 @@ var DebugMode bool
 var SiteDomain = "ons.gov.uk"
 
 // PatternLibraryAssetsPath is the URL to the CSS and JS assets from the pattern library
-var PatternLibraryAssetsPath = "//cdn.ons.gov.uk/sixteens/c463aaa"
+var PatternLibraryAssetsPath = "//cdn.ons.gov.uk/sixteens/6d75721"
