f0d9fe16e91c27d75451888a0466b1b2d290f643
mattrout92 <matt_rout92@live.co.uk>
2018-01-18 14:54:29 +0000

    Update hierarchy template for search

--- vendor/github.com/ONSdigital/dp-frontend-models/model/dataset-filter/hierarchy/hierarchy.go
+++ vendor/github.com/ONSdigital/dp-frontend-models/model/dataset-filter/hierarchy/hierarchy.go
@@ -11,19 +11,25 @@ type Page struct {
 
 // Hierarchy ...
 type Hierarchy struct {
-	Title         string   `json:"title"`
-	SaveAndReturn Link     `json:"save_and_return"`
-	Cancel        Link     `json:"cancel"`
-	FiltersAmount string   `json:"filters_amount"`
-	AddAllFilters AddAll   `json:"add_all"`
-	FilterList    []List   `json:"filter_list"`
-	FiltersAdded  []Filter `json:"filters_added"`
-	RemoveAll     Link     `json:"remove_all"`
-	GoBack        Link     `json:"go_back"`
-	DimensionName string   `json:"dimension_name"`
-	Parent        string   `json:"parent"`
-	Type          string   `json:"type"`
-	Metadata      Metadata `json:"metadata"`
+	Title           string   `json:"title"`
+	SaveAndReturn   Link     `json:"save_and_return"`
+	Cancel          Link     `json:"cancel"`
+	FiltersAmount   string   `json:"filters_amount"`
+	AddAllFilters   AddAll   `json:"add_all"`
+	FilterList      []List   `json:"filter_list"`
+	FiltersAdded    []Filter `json:"filters_added"`
+	RemoveAll       Link     `json:"remove_all"`
+	GoBack          Link     `json:"go_back"`
+	DimensionName   string   `json:"dimension_name"`
+	Parent          string   `json:"parent"`
+	Type            string   `json:"type"`
+	Metadata        Metadata `json:"metadata"`
+	DatasetTitle    string   `json:"dataset_title"`
+	SearchURL       string   `json:"search_url"`
+	IsSearchResults bool     `json:"is_search_results"`
+	Query           string   `json:"query"`
+	IsSearchError   bool     `json:"is_search_error"`
+	LandingPageURL  string   `json:"landing_page_url"`
 }
 
 // AddAll ...
