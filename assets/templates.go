f4776ac2bc771e0538c9e67c8560d6f2709a6151
boxadesign <info@boxadesign.co.uk>
2018-01-25 16:05:55 +0000

    reverted to a previous sixteens commit

--- assets/templates.go
+++ assets/templates.go
@@ -290,7 +290,7 @@ func templatesDatasetFilterPreviewPageTmpl() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "templates/dataset-filter/preview-page.tmpl", size: 7300, mode: os.FileMode(420), modTime: time.Unix(1516893130, 0)}
+	info := bindataFileInfo{name: "templates/dataset-filter/preview-page.tmpl", size: 7300, mode: os.FileMode(420), modTime: time.Unix(1516893209, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
