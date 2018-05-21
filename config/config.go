5075d03349018b473823eb1231cf89660a0ba74c
Crispioso <crispin.merriman@gmail.com>
2018-05-21 17:22:23 +0100

    Update to version eb938ca of Sixteens

--- config/config.go
+++ config/config.go
@@ -7,4 +7,4 @@ var DebugMode bool
 var SiteDomain = "ons.gov.uk"
 
 // PatternLibraryAssetsPath is the URL to the CSS and JS assets from the pattern library
-var PatternLibraryAssetsPath = "//cdn.ons.gov.uk/sixteens/1f31a7c"
+var PatternLibraryAssetsPath = "//cdn.ons.gov.uk/sixteens/eb938ca"
