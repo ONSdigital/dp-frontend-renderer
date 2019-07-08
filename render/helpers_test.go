b0304c1aaf93096907afad3db59151ef9e61be7d
Jon Dewi Jones <jondewijones@gmail.com>
2019-07-08 13:06:46 +0100

    Add more tests for markdown helper

--- render/helpers_test.go
+++ render/helpers_test.go
@@ -38,11 +38,31 @@ func TestHumanSize(t *testing.T) {
 	})
 }
 
-var testMarkdown = "- First bullet point\n\n- Second bullet point\n\n\tSecond line of second bullet point"
-var testHTML = "<ul>\n<li><p>First bullet point</p></li>\n\n<li><p>Second bullet point</p>\n\n<p>Second line of second bullet point</p></li>\n</ul>\n"
+var testMarkdown = []string{
+	"- First bullet point\n\n- Second bullet point\n\n\tSecond line of second bullet point",
+	"1. First bullet point\n\n2. Second bullet point",
+	"##Heading2",
+	"### Heading 3",
+	"#### Heading 4 \n\n Some text, _some italic text_, **some strong text**",
+	"[Test link](https://www.test.com)",
+	"### Heading 3 \n\n some text [Anchor link](#)",
+}
+
+// the new line at the end of each test case is needed
+var testHTML = []string{
+	"<ul>\n<li><p>First bullet point</p></li>\n\n<li><p>Second bullet point</p>\n\n<p>Second line of second bullet point</p></li>\n</ul>\n",
+	"<ol>\n<li><p>First bullet point</p></li>\n\n<li><p>Second bullet point</p></li>\n</ol>\n",
+	"<h2>Heading2</h2>\n",
+	"<h3>Heading 3</h3>\n",
+	"<h4>Heading 4</h4>\n\n<p>Some text, <em>some italic text</em>, <strong>some strong text</strong></p>\n",
+	"<p><a href=\"https://www.test.com\">Test link</a></p>\n",
+	"<h3>Heading 3</h3>\n\n<p>some text <a href=\"#\">Anchor link</a></p>\n",
+}
 
 func TestMarkdown(t *testing.T) {
 	Convey("markdown should return expected HTML", t, func() {
-		So(Markdown(testMarkdown), ShouldEqual, testHTML)
+		for i := 0; i < len(testMarkdown); i++ {
+			So(Markdown(testMarkdown[i]), ShouldEqual, testHTML[i])
+		}
 	})
 }
