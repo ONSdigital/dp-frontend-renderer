27c6bfaa5bfe0cc19e62e5b23fcc592e66aa5e44
Daniel W <danieldwalford@gmail.com>
2019-08-16 09:52:58 +0100

    Docker file management update
    * Build script for CI now creates an assets directory and adds locale files
    * Docker.concourse now copies all files rather than specifically specifying
    * ADD command replaced for COPY (as per best practice)
    *  Dockerfile now copies the locale files

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -5,6 +5,8 @@ export GOPATH=$(pwd)/go
 
 pushd $GOPATH/src/github.com/ONSdigital/dp-frontend-renderer
   go build -tags 'production' -o $BINPATH/dp-frontend-renderer
+  mkdir $BINPATH/assets
   cp taxonomy-redirects.yml $BINPATH/
   cp Dockerfile.concourse $BINPATH/
+  cp -r assets/locales $BINPATH/assets/locales
 popd
