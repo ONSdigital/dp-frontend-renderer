6a1f20af77e067c7a28832fa7ea72fed76e37863
Ravi Pradhan <ravipradhan@protonmail.com>
2020-02-12 15:39:17 +0000

    add conditional rendering to cookies banner

--- vendor/github.com/ONSdigital/dp-frontend-models/model/page.go
+++ vendor/github.com/ONSdigital/dp-frontend-models/model/page.go
@@ -20,4 +20,7 @@ type Page struct {
 	ReleaseDate                      string         `json:"release_date"`
 	BetaBannerEnabled                bool           `json:"beta_banner_enabled"`
 	EnableLoop11                     bool           `json:"enable_loop11"`
+	EnableCookiesControl             bool           `json:"enable_cookies_control"`
+	CookiesPreferencesSet            bool           `json:"cookies_preferences_set"`
+	CookiesPolicy                    CookiesPolicy  `json:"cookies_policy"`
 }
