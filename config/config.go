197a95968c40ed521407a4d896e1932acbef4f75
Ravi <ravipradhan@protonmail.com>
2020-06-02 15:05:53 +0100

    remove top padding from date and update sixteens cdn url

--- config/config.go
+++ config/config.go
@@ -32,7 +32,7 @@ func Get() (*Config, error) {
 	if cfg.Debug {
 		cfg.PatternLibraryAssetsPath = "http://localhost:9000/dist"
 	} else {
-		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/sixteens/a310eb3"
+		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/sixteens/6a9b674"
 
 	}
 	return cfg, nil
