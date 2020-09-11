3e07a078ec379780afd7760eda6e1fbaec35a8de
Tom Andrews <git@tomandrews.co.uk>
2020-09-11 09:59:40 +0100

    Update sixteens

--- config/config.go
+++ config/config.go
@@ -32,7 +32,7 @@ func Get() (*Config, error) {
 	if cfg.Debug {
 		cfg.PatternLibraryAssetsPath = "http://localhost:9000/dist"
 	} else {
-		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/sixteens/cd80c82"
+		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/sixteens/4d6c6f3"
 	}
 	return cfg, nil
 }
