63ffe75ed8e13ee793075ce59dfddc02c865f78a
Jon Dewi Jones <jondewijones@gmail.com>
2021-03-19 19:01:16 +0000

    Update sixteens

--- config/config.go
+++ config/config.go
@@ -30,7 +30,7 @@ func Get() (*Config, error) {
 	if cfg.Debug {
 		cfg.PatternLibraryAssetsPath = "http://localhost:9000/dist"
 	} else {
-		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/sixteens/b089aa4"
+		cfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/sixteens/bd9ce6c"
 	}
 	return cfg, nil
 }
