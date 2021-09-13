982e6b102023642a7211f1212f9cd39eb91d54e6
NickL <19624419+mr-nick17@users.noreply.github.com>
2021-09-13 16:01:39 +0100

    remove edition list template files and references

--- handlers/datasetLandingPage/handler.go
+++ handlers/datasetLandingPage/handler.go
@@ -1,12 +1,12 @@
 package datasetLandingPage
 
 import (
-	"github.com/ONSdigital/dp-frontend-renderer/config"
 	"net/http"
+
+	"github.com/ONSdigital/dp-frontend-renderer/config"
 
 	"github.com/ONSdigital/dp-frontend-models/model/datasetVersionsList"
 
-	"github.com/ONSdigital/dp-frontend-models/model/datasetEditionsList"
 	"github.com/ONSdigital/dp-frontend-models/model/datasetLandingPageFilterable"
 	"github.com/ONSdigital/dp-frontend-models/model/datasetLandingPageStatic"
 	"github.com/ONSdigital/dp-frontend-renderer/render"
@@ -38,15 +38,6 @@ func NomisHandler(cfg config.Config) http.HandlerFunc {
 		var page datasetLandingPageFilterable.Page
 
 		render.Handler(w, req, &page, &page.Page, "datasetLandingPage/nomis", nil, cfg)
-	}
-}
-
-//EditionListHandler ...
-func EditionListHandler(cfg config.Config) http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-		var page datasetEditionsList.Page
-
-		render.Handler(w, req, &page, &page.Page, "datasetLandingPage/edition-list", nil, cfg)
 	}
 }
 
