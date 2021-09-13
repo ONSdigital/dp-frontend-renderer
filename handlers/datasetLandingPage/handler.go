5e6e36a93be8c6a837f4106496a1625bfb99b637
NickL <19624419+mr-nick17@users.noreply.github.com>
2021-09-13 16:33:39 +0100

    remove static dlp template files and references

--- handlers/datasetLandingPage/handler.go
+++ handlers/datasetLandingPage/handler.go
@@ -7,20 +7,10 @@ import (
 
 	"github.com/ONSdigital/dp-frontend-models/model/datasetVersionsList"
 
-	"github.com/ONSdigital/dp-frontend-models/model/datasetLandingPageStatic"
 	"github.com/ONSdigital/dp-frontend-renderer/render"
 )
 
 const xRequestIDParam = "X-Request-Id"
-
-//StaticHandler builds the template for non-filterable dataset landing pages
-func StaticHandler(cfg config.Config) http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-		var page datasetLandingPageStatic.Page
-
-		render.Handler(w, req, &page, &page.Page, "datasetLandingPage/static", nil, cfg)
-	}
-}
 
 func VersionListHandler(cfg config.Config) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
